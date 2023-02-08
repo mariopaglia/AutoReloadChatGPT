9f9c9813e0cc7540119cebfef52af027b6ee9052
Mario Paglia <mario.junior@consorciei.com.br>
2023-02-08 11:07:02 -0300

    Version 0.2

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Force enter ChatGPT if it is "capacity right now"
 // @namespace    https://github.com/mariopaglia
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Mario Paglia
-// @updateURL    https://gist.githubusercontent.com/mariopaglia/ae5ac5ef156b2eaffd289f0245a674bd/raw/d8ef11c527a54802e273e67d00a116b18747c1c0/forcechatgpt.js
-// @downloadURL  https://gist.githubusercontent.com/mariopaglia/ae5ac5ef156b2eaffd289f0245a674bd/raw/d8ef11c527a54802e273e67d00a116b18747c1c0/forcechatgpt.js
+// @updateURL    https://github.com/mariopaglia/enterforce-chatgpt/raw/main/script.js
+// @downloadURL  https://github.com/mariopaglia/enterforce-chatgpt/raw/main/script.js
 // @match        https://chat.openai.com/auth/login
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chat.openai.com
 // @grant        none
